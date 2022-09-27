08e4da3a14b9b57bae86bd5ab008bb8493333c29
luqmanito <109712257+luqmanito@users.noreply.github.com>
2022-09-27 21:53:25 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,34 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
 import React from 'react';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <AppBar position="sticky" color="primary">
+      <Toolbar>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            paddingY: 2
+          }}
+        >
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Call a Friend
+          </Typography>
+          <Typography variant="subtitle1" noWrap component="div">
+            Your friendly contact app
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
