8245315698a17c91b1b027b05e49ff38ad89a600
luqmanito <109712257+luqmanito@users.noreply.github.com>
2022-09-27 21:52:36 +0700

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,31 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
+import { Avatar, Box, colors, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (<ListItem>
+        <ListItemAvatar>
+            <Avatar src={data.photo} />
+        </ListItemAvatar>
+        <ListItemText primary={data.name} secondary={
+            <>
+                <Box component="span" sx={{ display: 'block' }}>
+                    <Typography variant='subtitle1'>
+                        {data.phone}
+                    </Typography>
+                </Box>
+                <Box component="span" sx={{ display: 'block' }}>
+                    <Typography variant='subtitle1'>
+                        {data.email}
+                    </Typography>
+                </Box>
+            </>
+        } />
+    </ListItem>);
 };
 
 export default Contact;
