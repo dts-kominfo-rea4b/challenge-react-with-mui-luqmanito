d971262213e362b3f373697f6d0dba7d040b2d24
luqmanito <109712257+luqmanito@users.noreply.github.com>
2022-09-27 21:53:04 +0700

    update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,12 +3,74 @@
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
+import { Button, Card, CardActions, CardContent, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+const ContactForm = ({
+    onSubmit
+}) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [photo, setPhoto] = useState('');
 
+    const handleSubmit = () => {
+            onSubmit({
+            name,
+            phone,
+            email,
+            photo
+        })
+    }
     return (
-        <> </>
+        <Card sx={{}}>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Add Contact
+                </Typography>
+                <TextField
+                    id="name"
+                    label="Name"
+                    variant="standard"
+                    sx={{ width: '100%' }}
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <TextField
+                    id="phone"
+                    label="Phone"
+                    variant="standard"
+                    sx={{ width: '100%' }}
+                    required
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                />
+                <TextField
+                    id="email"
+                    label="Email"
+                    variant="standard"
+                    sx={{ width: '100%' }}
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                    id="photo"
+                    label="Photo URL"
+                    variant="standard"
+                    sx={{ width: '100%' }}
+                    required
+                    value={photo}
+                    onChange={(e) => setPhoto(e.target.value)}
+                />
+            </CardContent>
+            <CardActions>
+                <Button onClick={handleSubmit} size="small">Add</Button>
+            </CardActions>
+        </Card>
     );
 }
 
